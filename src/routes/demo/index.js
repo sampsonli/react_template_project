bd84b057d3a1efe380b6e4bd619108d5804d98a0
lichun <lichun@mistong.com>
2021-03-11 14:53:12 +0800

    add

--- src/routes/demo/index.js
+++ src/routes/demo/index.js
@@ -4,11 +4,13 @@ import load from '~/common/load';
 import UserWrap from '~/containers/UserWrap';
 
 const Demo = load(() => import('./pages/demo' /* webpackChunkName: demo */), UserWrap);
+const Rain = load(() => import('./pages/rain' /* webpackChunkName: rain */));
 export const init = true;
 export default () => {
     const match = useRouteMatch();
     return (
         <Switch>
+            <Route path={`${match.url}/rain`} component={Rain} />
             <Route path={`${match.url}`} component={Demo} />
         </Switch>
     );
