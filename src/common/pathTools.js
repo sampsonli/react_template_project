148689a4da0f9f6fe66e88508704054e4f187495
lichun <lichun@mistong.com>
2021-06-23 09:20:26 +0800

    optimate

---
+++ src/common/pathTools.js
@@ -0,0 +1,63 @@
+import {useMemo} from 'react';
+
+const resolvePath = (path) => {
+    path = path.replace(/\/\.\//g, '/')
+        .replace(/^\.\//, '');
+    const tmp = path.split('/');
+    const stack = [];
+    tmp.forEach((item) => {
+        if (item === '..') {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            stack.push(item);
+        }
+    });
+    return stack.join('/');
+};
+export const parseQueryStr = (qs) => {
+    if (qs && qs.indexOf('?') === 0) {
+        qs = qs.replace('?', '');
+    }
+    qs = qs || decodeURIComponent(window.location.hash.split('?')[1]);
+    const result = {};
+    if (qs) {
+        qs.split('&')
+            .forEach(ele => {
+                const tmp = ele.split('=');
+                // eslint-disable-next-line prefer-destructuring
+                result[tmp[0]] = tmp[1];
+            });
+    }
+    return result;
+};
+export const useQueryParams = () => useMemo(parseQueryStr, [window.location.href]);
+
+const getFinalPath = (path, query = {}) => {
+    const queryStr = Object.keys(query)
+        .map(key => `${key}=${query[key]}`)
+        .join('&');
+    if (queryStr) {
+        if (path.indexOf('/') === 0) {
+            return `${resolvePath(`#${path}`)}?${queryStr}`;
+        }
+        const fPath = window.location.hash.substr(0, window.location.hash.lastIndexOf('/') + 1) + path;
+        return `${resolvePath(fPath)}?${queryStr}`;
+    }
+    if (path.indexOf('/') === 0) {
+        return resolvePath(`#${path}`);
+    }
+    const fPath = window.location.hash.substr(0, window.location.hash.lastIndexOf('/') + 1) + path;
+    return resolvePath(fPath);
+};
+
+export const pushPath = (path, query = {}) => {
+    window.location.href = getFinalPath(path, query);
+};
+export const replacePath = (path, query = {}) => {
+    window.location.replace(getFinalPath(path, query));
+};
+export const back = () => {
+    window.history.back();
+};
--- src/routes/demo/pages/demo/index.js
+++ src/routes/demo/pages/demo/index.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {useModel} from 'redux-spring';
 import style from './style.less';
 import DemoModel from '~/routes/demo/models/DemoModel';
+import {pushPath} from '~/common/pathTools';
 
 export default () => {
     const model = useModel(DemoModel);
@@ -15,7 +16,7 @@ export default () => {
         <div className={style.container}>
             <div className={style.content}>
                 <div className={style.add} onClick={model.addOne}>+</div>
-                <div className={style.num}>{loaded ? num : '加载中'}</div>
+                <div className={style.num} onClick={() => pushPath('./demo/rain', {name: 'hello'})}>{loaded ? num : '加载中'}</div>
                 <div className={style.minus} onClick={model.minusOne}>-</div>
             </div>
         </div>
