c9669b1c5d4db09899b9fc258775e623107a3647
李春 <lichun5@sfmail.sf-express.com>
2021-11-20 16:17:42 +0800

    optimate

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
 /** postcss-loader 解析器所需的配置文件 * */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const autoprefixer = require('autoprefixer');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const pxtorem = require('postcss-pxtorem');
+
 module.exports = {
     plugins: [
-      // eslint-disable-next-line import/no-extraneous-dependencies
-      require('autoprefixer'),
+      autoprefixer,
+        pxtorem({
+            replace: true,
+            propList: ['font', 'font-size', 'line-height', 'letter-spacing', 'margin*', 'padding*',
+                'width', 'height', 'left', 'top', 'right', 'bottom', 'transform', 'box-shadow'],
+            rootValue: 100,
+        }),
     ],
 };
