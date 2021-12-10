262826f6573f2d36cd9dcee1b35f86c88b4bad36
李春 <740056710@qq.com>
2021-12-10 13:48:54 +0800

    optimate

---
+++ src/routes/pc/pages/Demo1/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useModel } from 'redux-spring';
+import style from './style.less';
+import DemoModel from '~/routes/pc/models/DemoModel';
+
+export default () => {
+    const model = useModel(DemoModel);
+    useEffect(() => {
+        model.init();
+    }, []);
+    return (
+        <div className={style.container}>
+            <div className={style.content}>
+
+                demo1
+            </div>
+        </div>
+
+    );
+};
