cf3999129a96f21a1a9d572b920fc652d9d2a866
李春 <lichun5@sfmail.sf-express.com>
2021-12-28 20:29:32 +0800

    fixbug

--- src/routes/pc/pages/Test/index.js
+++ src/routes/pc/pages/Test/index.js
@@ -4,18 +4,21 @@ import {useInitModel} from 'mtor';
 
 import TestModel from '~/routes/pc/models/TestModel';
 import style from './style.less';
+import {Button} from 'antd';
 
 export default () => {
     const model = useInitModel(TestModel, () => {
         // model.init();
-    });
+    }, true);
     return (
-        <div className={style.container} onClick={model.asyncFnDemo}>
+        <div className={style.container}>
+            <Button onClick={() => model.asyncFnDemo()}>开始</Button>
             {model.num}
             <br/>
             {model.num2}
             <br/>
             {model.num3}
+            <Button onClick={() => model.reset()}>清除</Button>
         </div>
 
     );
