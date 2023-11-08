d7ae41fa4374cef0e4bd0f2b6bc93afba7950991
李春 <lichun5@sfmail.sf-express.com>
2023-11-08 16:45:27 +0800

    optimate

--- src/routes/pc/models/TestModel.js
+++ src/routes/pc/models/TestModel.js
@@ -17,9 +17,88 @@ export const all = (() => {
 class Demo2Model extends Model {
     loaded = false;
 
+    dataView;
+
     allElements = [];
 
     video;
+
+    generateFromVideo() {
+        return new Promise((resolve => {
+            const {allElements} = this;
+            const video = document.createElement('video');
+            this.video = video;
+            video.autoplay = true;
+            video.src = 'Badapplesmall.mp4';
+            const canvas = document.createElement('canvas');
+            canvas.width = allElements[0].length;
+            canvas.height = allElements.length;
+            const ctx = canvas.getContext('2d');
+            const dataView = new DataView(new ArrayBuffer(60 * 80 / 8 * 30 * 240));
+            let index = 0;
+            let offset = 0;
+            let byte = 0;
+            let flag = true; // 保持30fps
+            function render() {
+                // if (video.ended || index > 100000) {
+                if (video.ended) {
+                    const dv = new DataView(dataView.buffer, 0, index);
+                    resolve(dv);
+                    video.pause();
+                    return;
+                }
+                window.requestAnimationFrame(render);
+                if (flag) {
+                    flag = false;
+                    return;
+                }
+                flag = true;
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
+                allElements.forEach((row, i) => {
+                    row.forEach((ele, j) => {
+                        const image = ctx.getImageData(j, i, 1, 1);
+                        if (image.data[0] <= 80) {
+                            ele.classList.add('current');
+                            byte |= 2 ** offset;
+                        } else {
+                            ele.classList.remove('current');
+                        }
+                        offset++;
+                        if (offset === 8) {
+                            offset = 0;
+                            dataView.setUint8(index, byte);
+                            byte = 0;
+                            index++;
+                        }
+                    });
+                });
+            }
+            video.onplay = () => {
+                render();
+            };
+        }));
+    }
+
+    async loadFromFile() {
+        return new Promise((resolve => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', 'mp4.blob');
+            xhr.responseType = 'arraybuffer';
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
+                    /**
+                     *
+                     * @type {ArrayBuffer}
+                     */
+                    const data = xhr.response;
+                    resolve(new DataView(data));
+                }
+            };
+            xhr.send();
+        }));
+    }
 
     init() {
         const allElements = [];
@@ -31,42 +110,58 @@ class Demo2Model extends Model {
             });
         });
         this.allElements = allElements;
-        const video = document.createElement('video');
-        this.video = video;
-        video.autoplay = true;
-        video.src = 'Badapplesmall.mp4';
-        const canvas = document.createElement('canvas');
-        canvas.width = allElements[0].length;
-        canvas.height = allElements.length;
-        const ctx = canvas.getContext('2d');
+    }
+
+    async playAndSaveFile() {
+        const dataView = await this.generateFromVideo();
+        const blob = new Blob([new Uint8Array(dataView.buffer, 0, dataView.byteLength)]);
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'mp4.blob';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+    }
+
+    async play() {
+        const {allElements} = this;
+        const dataView = await this.loadFromFile();
+
+        let index = 0;
+        let offset = 0;
+        let byte = dataView.getUint8(index);
         let flag = true; // 保持30fps
         function render() {
+            if (index >= dataView.byteLength) return;
             window.requestAnimationFrame(render);
             if (flag) {
                 flag = false;
                 return;
             }
-                flag = true;
-
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
-
-            allElements.forEach((row, i) => {
-                row.forEach((ele, j) => {
-                    const image = ctx.getImageData(j, i, 1, 1);
-                    if (image.data[0] <= 80) {
+            flag = true;
+            allElements.forEach((row) => {
+                row.forEach((ele) => {
+                    if (byte & (2 ** offset)) {
                         ele.classList.add('current');
                     } else {
                         ele.classList.remove('current');
+                    }
+                    offset++;
+                    if (offset === 8) {
+                        index++;
+                        offset = 0;
+                        if (index >= dataView.byteLength) {
+                            byte = 0;
+                        } else {
+                            byte = dataView.getUint8(index);
+                        }
                     }
                 });
             });
         }
         render();
-    }
-
-    play() {
-        this.video.play();
     }
 }
 export default Demo2Model;
--- src/routes/pc/pages/Test/index.js
+++ src/routes/pc/pages/Test/index.js
@@ -6,14 +6,18 @@ import TestModel, {all} from '~/routes/pc/models/TestModel';
 import style from './style.less';
 
 export default () => {
-    const ct = useRef();
     const model = useInitModel(TestModel, () => {
         model.init();
+        model.play();
     }, true);
     return (
         <div className={style.container}>
-            <div className={style.content} ref={ct}>
-                <div className={style.btn} onClick={model.play}>播放</div>
+            <div className={`${style.content} ${window._isMobile ? style.mobile : ''}`}>
+                <div className={style.opts}>
+                    <div className={style.btn} onClick={model.play}>播放</div>
+                    <div className={style.btn} onClick={model.playAndSaveFile}>播放视频并保存</div>
+                </div>
+
                 {all.map((row) => (
                     <div key={row.key} className={style.row}>
                         {row.map(col => <div key={col} id={col} className={`${style.col} current`} />)}
