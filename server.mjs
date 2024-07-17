5cf7c00da7b3bbeab423817e87b9d05029597145
lichun <740056710@qq.com>
2024-07-17 14:28:02 +0800

    upgrade

--- server.mjs
+++ server.mjs
@@ -1,16 +1,11 @@
 /** 用于开发环境的服务启动 * */
-// eslint-disable-next-line
-const express = require('express');
-// eslint-disable-next-line
-const undici = require('undici');
-// eslint-disable-next-line
-const bodyParser = require('body-parser');
-// eslint-disable-next-line
-const http = require('http');
-// eslint-disable-next-line
-const compression = require('compression');
-// eslint-disable-next-line
-const historyApiFallback = require('connect-history-api-fallback');
+import express from 'express';
+import undici from 'undici';
+import bodyParser from 'body-parser';
+import http from 'node:http';
+import compression from 'compression'
+import historyApiFallback from 'connect-history-api-fallback';
+
 
 const app = express(); // 实例化express服务
 app.use(bodyParser.json());
@@ -18,15 +13,15 @@ const { PORT = 8816} = process.env; // 服务启动端口号
 app.use(historyApiFallback());
 // 如果是生产环境，则运行build文件夹中的代码
 app.use(compression());
-/* app.use(express.static('dist', {
+app.use(express.static('dist', {
     maxAge: '1d',
     setHeaders(res, file) {
         if (file.indexOf('index.html') > -1) {
             res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=5');
         }
     },
-})); */
-app.use(express.static('dist'));
+}));
+// app.use(express.static('dist'));
 
 app.use((req, resp, next) => {
     if (req.originalUrl.indexOf('/user') > -1) {
